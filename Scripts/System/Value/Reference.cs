42e2a94726bb6333fbd3349edd7aec32fae9c639
紫地丁 <jianhaoli@lilith.com>
2024-10-14 20:08:16 +0800

    [feat]数据系统

---
+++ Scripts/System/Value/Reference.cs
@@ -0,0 +1,15 @@
+// -------------------------------------------------
+// Copyright@ The Most Devout Followers of McDonald
+// Author : lijianhao
+// Date: 2024_10_14
+// -------------------------------------------------
+
+using UnityEngine;
+
+namespace PurpleFlowerCore.Utility
+{
+    public class Reference : MonoBehaviour
+    {
+        
+    }
+}
--- Scripts/System/Value/Value.cs
+++ Scripts/System/Value/Value.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace PurpleFlowerCore.Utility
 {
-    public class Value<T>
+    [Serializable]
+    public class Value<T> where T : struct , IComparable<T>
     {
         /// <summary>
         /// 标识, 目前仅用于调试, 未来可能会用于其他用途
@@ -11,22 +14,23 @@ namespace PurpleFlowerCore.Utility
         /// <summary>
         /// 具体值
         /// </summary>
-        private T _value;
+        [SerializeField]private T _value;
         /// <summary>
         /// 是否可读, 默认为true
         /// </summary>
-        private bool _canRead;
+        [SerializeField]private bool _canRead;
         /// <summary>
         /// 是否可写, 默认为true
         /// </summary>
-        private bool _canWrite;
+        [SerializeField]private bool _canWrite;
         /// <summary>
         /// 是否可传入更改,如+= *=等, 默认为true
         /// </summary>
-        private bool _canChange;
-        private T _min;
-        private T _max;
-        public event Action<T,T> OnValueChange;
+        [SerializeField]private bool _canChange;
+        [SerializeField]private T _min;
+        [SerializeField]private T _max;
+        
+        public UnityEvent<T,T> OnValueChange;//todo:是否使用UnityEvent
         public event Action<T,T> OnValueChanged;
         public event Action<T> OnValueWrite;
         public event Action<T,T> OnValueAdd;
@@ -42,8 +46,18 @@ namespace PurpleFlowerCore.Utility
             get => _min;
             set
             {
-                if (CompareValues(value, _max) > 0) throw new Exception("MinValue must be less than MaxValue");
+                //if (CompareValues(value, _max) > 0) throw new Exception("MinValue must be less than MaxValue");
                 _min = value;
+            }
+        }
+        
+        public T Max
+        {
+            get => _max;
+            set
+            {
+                //if (CompareValues(value, _min) < 0) throw new Exception("MaxValue must be greater than MinValue");
+                _max = value;
             }
         }
         
@@ -54,33 +68,43 @@ namespace PurpleFlowerCore.Utility
             _canRead = canRead;
             _canWrite = canWrite;
             _canChange = canChange;
-            if (IsIComparable<T>())
-            {
-                _min = min;
-                _max = max;
-                if (CompareValues(value, min) < 0)
-                {
-                    // todo: 初始化时事件为空
-                    OnValueReachMin?.Invoke(value);
-                    _value = min;
-                }
-                if (CompareValues(value, max) > 0)
-                {
-                    OnValueReachMax?.Invoke(value);
-                    _value = max;
-                }
-            }
-            else
-            {
-                _min = default;
-                _max = default;
-                throw new Exception("Value must be IComparable");
-            }
-
+            // if (IsIComparable<T>())
+            // {
+            //     _min = min;
+            //     _max = max;
+            //     if (CompareValues(value, min) < 0)
+            //     {
+            //         // todo: 初始化时事件为空
+            //         OnValueReachMin?.Invoke(value);
+            //         _value = min;
+            //     }
+            //     if (CompareValues(value, max) > 0)
+            //     {
+            //         OnValueReachMax?.Invoke(value);
+            //         _value = max;
+            //     }
+            // }
+            // else
+            // {
+            //     _min = default;
+            //     _max = default;
+            //     throw new Exception("Value must be IComparable");
+            // }
+            // if (value < min)
+            // {
+            //     // todo: 初始化时事件为空
+            //     OnValueReachMin?.Invoke(value);
+            //     _value = min;
+            // }
+            // if (CompareValues(value, max) > 0)
+            // {
+            //     OnValueReachMax?.Invoke(value);
+            //     _value = max;
+            // }
         }
         
         // todo: 该属性存在的必要性,是否可以被运算符重载替代
-        public T TheValue
+        public T value
         {
             get
             {
@@ -90,27 +114,30 @@ namespace PurpleFlowerCore.Utility
             set
             {
                 if(!_canWrite) throw new Exception($"Can't write this value: {_name}");
-                if(IsIComparable<T>()) throw new Exception($"Value must be greater than {_min}");
+                //todo:最大最小判断
                 OnValueChange?.Invoke(_value, value);
                 _value = value;
                 OnValueChanged?.Invoke(_value, value);
             }
         }
         
-        private static bool IsIComparable<T>()
-        {
-            return typeof(IComparable).IsAssignableFrom(typeof(T)) ||
-                   typeof(IComparable<T>).IsAssignableFrom(typeof(T));
-        }
         
-        private static int CompareValues<T>(T x, T y)
-        {
-            if (x is IComparable comparableX)
-            {
-                return comparableX.CompareTo(y);
-            }
-
-            throw new ArgumentException($"{typeof(T).FullName} must implement IComparable.");
-        }
+        
+        // // todo: 支持不可比较类型的必要性
+        // private static bool IsIComparable<T>()
+        // {
+        //     return typeof(IComparable).IsAssignableFrom(typeof(T)) ||
+        //            typeof(IComparable<T>).IsAssignableFrom(typeof(T));
+        // }
+        //
+        // private static int CompareValues<T>(T x, T y)
+        // {
+        //     if (x is IComparable comparableX)
+        //     {
+        //         return comparableX.CompareTo(y);
+        //     }
+        //
+        //     throw new ArgumentException($"{typeof(T).FullName} must implement IComparable.");
+        // }
     }
 }
