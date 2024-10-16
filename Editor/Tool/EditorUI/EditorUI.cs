f7c396becef96dacaccfb70723a87b5b97a9743b
紫地丁 <jianhaoli@lilith.com>
2024-10-16 19:05:48 +0800

    [feat]EditorUI

--- Editor/Tool/EditorUI/EditorUI.cs
+++ Editor/Tool/EditorUI/EditorUI.cs
@@ -5,12 +5,27 @@ namespace PurpleFlowerCore.Editor.Tool
 {
     public static class EditorUI
     {
-        public static void ShowInt(ref int value, string label)
+        public static void ShowInt(string label, ref int value, GUIStyle style = null, params GUILayoutOption[] option)
         {
-            GUILayout.BeginHorizontal();
             GUILayout.Label(label, GUILayout.Width(100));
-            value = EditorGUILayout.IntField(value);
-            GUILayout.EndHorizontal();   
+            value = EditorGUILayout.IntField(label, value, style, option);
+        }
+    }
+    
+    public class TestWindow : EditorWindow
+    {
+        private int _testValue = 0;
+        
+        [MenuItem("PFC/TestWindow")]
+        public static void ShowWindow()
+        {
+            GetWindow<TestWindow>("TestWindow");
+        }
+
+        private void OnGUI()
+        {
+            EditorUI.ShowInt("Test Value", ref _testValue);
+            PFCLog.Info("TestWindow" + _testValue);
         }
     }
     
