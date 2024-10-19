c9b644cc1bd3e352c104c0ee4772544a8a51071e
紫地丁 <jianhaoli@lilith.com>
2024-10-19 15:37:22 +0800

    [feat]添加MoveTowards

--- src/Runtime/Utility/ScaleUtility.cs
+++ src/Runtime/Utility/ScaleUtility.cs
@@ -20,18 +20,38 @@ namespace PurpleFlowerCore.Utility
             transformBuffer.Remove(transform);
         }
         
-        public static void LerpTo(Vector3 target, Transform transform, float speed, UnityAction callBack = null)
+        public static void Lerp(Vector3 target, Transform transform, float speed, UnityAction callBack = null)
         {
             if (transformBuffer.Contains(transform)) return;
             AddBuffer(transform);
-            MonoSystem.Start_Coroutine(DoLerpTo(target, transform, speed, callBack));
+            MonoSystem.Start_Coroutine(DoLerp(target, transform, speed, callBack));
         }
 
-        private static IEnumerator DoLerpTo(Vector3 target, Transform transform, float speed, UnityAction callBack)
+        private static IEnumerator DoLerp(Vector3 target, Transform transform, float speed, UnityAction callBack)
         {
             while (Mathf.Abs(transform.localScale.x-target.x)>0.01f)
             {
                 transform.localScale = Vector3.Lerp(transform.localScale, target, 1/speed);
+                yield return new WaitForSeconds(0.01f);
+            }
+            transform.localScale = target;
+            RemoveBuffer(transform);
+            callBack?.Invoke();
+            
+        }
+        
+        public static void MoveTowards(Vector3 target, Transform transform, float speed, UnityAction callBack = null)
+        {
+            if (transformBuffer.Contains(transform)) return;
+            AddBuffer(transform);
+            MonoSystem.Start_Coroutine(DoMoveTowards(target, transform, speed, callBack));
+        }
+        
+        private static IEnumerator DoMoveTowards(Vector3 target, Transform transform, float speed, UnityAction callBack)
+        {
+            while (Vector3.Distance(transform.localScale, target)>0.01f)
+            {
+                transform.localScale = Vector3.MoveTowards(transform.localScale, target, speed);
                 yield return new WaitForSeconds(0.01f);
             }
             transform.localScale = target;
