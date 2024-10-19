3aa8bee46b8a07169e74f2a94b5c15d02df5698c
紫地丁 <jianhaoli@lilith.com>
2024-10-19 14:43:36 +0800

    [fix]完善ScaleUtility

--- src/Runtime/Utility/ScaleUtility.cs
+++ src/Runtime/Utility/ScaleUtility.cs
@@ -20,20 +20,21 @@ namespace PurpleFlowerCore.Utility
             transformBuffer.Remove(transform);
         }
         
-        
         public static void LerpTo(Vector3 target, Transform transform, float speed, UnityAction callBack = null)
         {
+            if (transformBuffer.Contains(transform)) return;
             AddBuffer(transform);
             MonoSystem.Start_Coroutine(DoLerpTo(target, transform, speed, callBack));
         }
 
         private static IEnumerator DoLerpTo(Vector3 target, Transform transform, float speed, UnityAction callBack)
         {
-            while (!transform.localScale.x.Equals(target.x))
+            while (Mathf.Abs(transform.localScale.x-target.x)>0.01f)
             {
                 transform.localScale = Vector3.Lerp(transform.localScale, target, 1/speed);
                 yield return new WaitForSeconds(0.01f);
             }
+            transform.localScale = target;
             RemoveBuffer(transform);
             callBack?.Invoke();
         }
