adecdf115de203feefb82a2b1557230c0e72bd90
紫地丁 <jianhaoli@lilith.com>
2024-10-19 13:15:35 +0800

    [fix]ScaleUtility保护

--- src/Runtime/Utility/ScaleUtility.cs
+++ src/Runtime/Utility/ScaleUtility.cs
@@ -1,13 +1,29 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace PurpleFlowerCore.Utility
 {
-    public class ScaleUtility
+    public static class ScaleUtility
     {
+        public static HashSet<Transform> transformBuffer = new();
+
+        public static void AddBuffer(Transform transform)
+        {
+            transformBuffer.Add(transform);
+        }
+
+        public static void RemoveBuffer(Transform transform)
+        {
+            if(!transformBuffer.Contains(transform)) return;
+            transformBuffer.Remove(transform);
+        }
+        
+        
         public static void LerpTo(Vector3 target, Transform transform, float speed, UnityAction callBack = null)
         {
+            AddBuffer(transform);
             MonoSystem.Start_Coroutine(DoLerpTo(target, transform, speed, callBack));
         }
 
@@ -18,6 +34,7 @@ namespace PurpleFlowerCore.Utility
                 transform.localScale = Vector3.Lerp(transform.localScale, target, 1/speed);
                 yield return new WaitForSeconds(0.01f);
             }
+            RemoveBuffer(transform);
             callBack?.Invoke();
         }
     }
