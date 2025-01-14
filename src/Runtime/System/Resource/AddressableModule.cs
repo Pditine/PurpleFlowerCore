9c01b8dfdefc2b01f40f9e8f9121d3ca6c32b5a1
紫地丁 <hanfe666@outlook.com>
2025-01-15 00:59:05 +0800

    [feat] Addressable加载多资源

--- src/Runtime/System/Resource/AddressableModule.cs
+++ src/Runtime/System/Resource/AddressableModule.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
@@ -51,6 +51,109 @@ namespace PurpleFlowerCore.Resource
             };
             _resDic.Add(keyName, handle);
         }
+
+        /// <summary>
+        /// 加载多个或指定资源
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <param name="callBack"></param>
+        /// <param name="keys"></param>
+        /// <typeparam name="T"></typeparam>
+        public void Load<T>(Addressables.MergeMode mode, Action<T> callBack, params string[] keys)
+        {
+            List<string> list = new();
+            foreach (var key in keys)
+            {
+                list.Add(key);
+            }
+            AsyncOperationHandle<IList<T>> handle;
+            string keyName = keys.Aggregate("", (current, key) => current + (key + "_"));
+            keyName += typeof(T).Name;
+            if (_resDic.ContainsKey(keyName))
+            {
+                handle = (AsyncOperationHandle<IList<T>>)_resDic[keyName];
+                if (handle.IsDone)
+                {
+                    foreach (var res in handle.Result)
+                    {
+                        callBack(res);
+                    }
+                }
+                else
+                {
+                    handle.Completed += operation =>
+                    {
+                        if (operation.Status == AsyncOperationStatus.Succeeded)
+                        {
+                            foreach (var res in handle.Result)
+                            {
+                                callBack(res);
+                            }
+                        }
+                    };
+                }
+                return;
+            }
+            
+            handle = Addressables.LoadAssetsAsync(list, callBack, mode);
+            handle.Completed += operation =>
+            {
+                if (operation.Status == AsyncOperationStatus.Failed)
+                {
+                    PFCLog.Error("Addressable", $"LoadAssetsAsync Failed: {keyName}");
+                    if(_resDic.ContainsKey(keyName))
+                        _resDic.Remove(keyName);
+                }
+            };
+            _resDic.Add(keyName, handle);
+        }
+        
+        public void Load<T>(Addressables.MergeMode mode, Action<AsyncOperationHandle<IList<T>>> callBack, params string[] keys)
+        {
+            List<string> list = new();
+            foreach (var key in keys)
+            {
+                list.Add(key);
+            }
+            AsyncOperationHandle<IList<T>> handle;
+            string keyName = keys.Aggregate("", (current, key) => current + (key + "_"));
+            keyName += typeof(T).Name;
+            if (_resDic.ContainsKey(keyName))
+            {
+                handle = (AsyncOperationHandle<IList<T>>)_resDic[keyName];
+                if (handle.IsDone)
+                {
+                    callBack(handle);
+                }
+                else
+                {
+                    handle.Completed += operation =>
+                    {
+                        if (operation.Status == AsyncOperationStatus.Succeeded)
+                        {
+                            callBack(handle);
+                        }
+                    };
+                }
+                return;
+            }
+            
+            handle = Addressables.LoadAssetsAsync<T>(list, obj => {}, mode);
+            handle.Completed += operation =>
+            {
+                if (operation.Status == AsyncOperationStatus.Failed)
+                {
+                    PFCLog.Error("Addressable", $"LoadAssetsAsync Failed: {keyName}");
+                    if(_resDic.ContainsKey(keyName))
+                        _resDic.Remove(keyName);
+                }
+                if (operation.Status == AsyncOperationStatus.Succeeded)
+                {
+                    callBack(handle);
+                }
+            };
+            _resDic.Add(keyName, handle);
+        }
         
         public void Release<T>(string name)
         {
@@ -58,6 +161,18 @@ namespace PurpleFlowerCore.Resource
             if (_resDic.ContainsKey(keyName))
             {
                 AsyncOperationHandle<T> handle = (AsyncOperationHandle<T>)_resDic[keyName];
+                Addressables.Release(handle);
+                _resDic.Remove(keyName);
+            }
+        }
+
+        public void Release<T>(params string[] keys)
+        {
+            string keyName = keys.Aggregate("", (current, key) => current + (key + "_"));
+            keyName += typeof(T).Name;
+            if (_resDic.ContainsKey(keyName))
+            {
+                AsyncOperationHandle<IList<T>> handle = (AsyncOperationHandle<IList<T>>)_resDic[keyName];
                 Addressables.Release(handle);
                 _resDic.Remove(keyName);
             }
